6e197ee29aef843457935f0511ef4bb807c51d02
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2025-06-12 22:57:57 +0500

    updated

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,4 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
-import { registerLicense } from "@syncfusion/ej2-base";
-registerLicense(
-  "Ngo9BigBOggjHTQxAR8/V1NNaF5cWWNCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXpedXRXRmJZVEZ2X0tWYUA="
-);
 
 export default [
   layout("routes/admin/admin-layout.tsx", [
